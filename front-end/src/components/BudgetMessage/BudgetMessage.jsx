7afed0fce91640e26830f71fcd128a3bcda978ef
XuanThao03 <vxuanthao03@gmail.com>
2026-02-12 15:34:38 +0700

    budget message component

---
+++ front-end/src/components/BudgetMessage/BudgetMessage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function BudgetMessage({
+  title,
+  message,
+  textColor,
+  bgColor,
+  width,
+}) {
+  return (
+    <div
+      className={`px-6 py-7 rounded-2xl bg-success justify-items-start  ${bgColor} ${width}`}
+    >
+      <p className={`text-base font-semibold ${textColor}`}>{title}</p>
+      <p className={`text-base font-normal text-left ${textColor}`}>
+        {message}
+      </p>
+    </div>
+  );
+}
