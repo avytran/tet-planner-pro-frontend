03b2acda44a707ea8bd64e66dfbaebc7fd357d4a
Minh Trang <20522039@gm.uit.edu.vn>
2026-02-15 13:30:17 +0700

    init

--- front-end/src/pages/DashboardPage/DashboardPage.jsx
+++ front-end/src/pages/DashboardPage/DashboardPage.jsx
@@ -1,8 +1,17 @@
+import './DashboardPage.css';
 export default function DashboardPage() {
+  const date = new Date();
+  const dateArray = date.toDateString().split(" ");
+  const time = date.toLocaleTimeString("en-US", {
+    hour: '2-digit',
+    minute: '2-digit'
+  });
   return (
-    <div>
-      <h2>Dashboard</h2>
-      <p>Trang tổng quan.</p>
+    <div className="container">
+      <div className="dashboardTitle">
+        <p className="text-5xl font-semibold text-primary">Dashboard</p>
+        <p className='text-black'>{time} - {dateArray.slice(1, dateArray.length - 1).join(" ")}, {dateArray[dateArray.length - 1]}</p>
+      </div>
     </div>
   );
 }
