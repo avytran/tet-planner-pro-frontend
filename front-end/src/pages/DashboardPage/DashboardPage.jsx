b6b2f8e0a81227f660ef0d49ad3af3a74aedc6f8
Minh Trang <20522039@gm.uit.edu.vn>
2026-02-16 18:24:44 +0700

    rename

--- front-end/src/pages/DashboardPage/DashboardPage.jsx
+++ front-end/src/pages/DashboardPage/DashboardPage.jsx
@@ -7,11 +7,12 @@ export default function DashboardPage() {
     minute: '2-digit'
   });
   return (
-    <div className="container">
+    <div className="dashboard-container">
       <div className="dashboardTitle">
         <p className="text-5xl font-semibold text-primary">Dashboard</p>
         <p className='text-black'>{time} - {dateArray.slice(1, dateArray.length - 1).join(" ")}, {dateArray[dateArray.length - 1]}</p>
       </div>
+      
     </div>
   );
 }
