3db923f5170379a6c93ceac557aca85614972470
avytran <tr.anhvy.work@gmail.com>
2026-02-22 17:17:15 +0700

    feat: apply apollo provider to app

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import { ApolloProvider } from "@apollo/client/react";
+import { apolloClient } from "./apollo/client.js";
 document.documentElement.setAttribute("data-theme", "apricot");
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ApolloProvider client={apolloClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
   </StrictMode>
 );
