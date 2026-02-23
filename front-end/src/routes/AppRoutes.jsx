a9798083d7977b2c0682f879c78fac9145d07255
avytran <tr.anhvy.work@gmail.com>
2026-02-24 00:22:22 +0700

    feat: implement auth protected route

--- front-end/src/routes/AppRoutes.jsx
+++ front-end/src/routes/AppRoutes.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { ProtectedRoute } from "./ProtectedRoute";
 
 import MainLayout from "../layouts/MainLayout";
 import AuthLayout from "../layouts/AuthLayout/AuthLayout";
@@ -15,13 +16,15 @@ import { ResetPasswordPage } from "../pages/ResetPasswordPage/ResetPasswordPage"
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<DashboardPage />} />
-        <Route path="dashboard" element={<DashboardPage />} />
-        <Route path="task-management" element={<TaskManagementPage />} />
-        <Route path="budget-management" element={<BudgetManagementPage />} />
-        <Route path="shopping-list" element={<ShoppingListPage />} />
-        <Route path="about" element={<AboutPage />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<DashboardPage />} />
+          <Route path="dashboard" element={<DashboardPage />} />
+          <Route path="task-management" element={<TaskManagementPage />} />
+          <Route path="budget-management" element={<BudgetManagementPage />} />
+          <Route path="shopping-list" element={<ShoppingListPage />} />
+          <Route path="about" element={<AboutPage />} />
+        </Route>
       </Route>
       <Route element={<AuthLayout />}>
         <Route path="login" element={<LoginPage />} />
