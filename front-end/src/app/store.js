00d1d1500ab998ad6ba82ff08b9e35d651b144f0
avytran <tr.anhvy.work@gmail.com>
2026-03-01 01:20:55 +0700

    feat: add undo redo slice redux

--- front-end/src/app/store.js
+++ front-end/src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import budgetReducer from "../features/budget/budgetSlice";
 import shoppingListReducer from "../features/shoppingList/shoppingListSlice";
+import undoRedoReducer from "../features/undoRedo/undoRedoSlice";
 export default configureStore({
   reducer: {
     budget: budgetReducer,
     shoppingList: shoppingListReducer,
+    undoRedo: undoRedoReducer,
   },
 });
